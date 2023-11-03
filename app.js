781f161ec708af00dd762f32c3e35266f723429d
Motaz HM <motazhajm@gmail.com>
2023-11-03 02:35:12 +0200

    Add base response

---
+++ app.js
@@ -0,0 +1,16 @@
+const { timeStamp } = require("console");
+const http = require("http");
+
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + timeStamp() + "<h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello, world!<h1>");
+  }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
