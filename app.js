26878fd068deab5bb98339844dfc4a18dea3042c
Motaz HM <motazhajm@gmail.com>
2023-11-03 02:38:29 +0200

    Add path examples

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const http = require("http");
 function handleRequest(request, response) {
   if (request.url === "/currenttime") {
     response.statusCode = 200;
-    response.end("<h1>" + timeStamp() + "<h1>");
+    response.end("<h1>" + new Date().toISOString() + "<h1>");
   } else if (request.url === "/") {
     response.statusCode = 200;
     response.end("<h1>Hello, world!<h1>");
+  } else {
+    response.statusCode = 404;
+    response.end("<h1>404 Not Found<h1>");
   }
 }
 
