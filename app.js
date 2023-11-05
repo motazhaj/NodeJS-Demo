d28faf9b59939069089bbf4a27d18a6fdffd10d5
Motaz HM <motazhajm@gmail.com>
2023-11-05 10:18:46 +0200

    Convert to node served site

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 const fs = require("fs");
 const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/currenttime", (req, res) => {
-  res.send("<h1>" + new Date().toISOString() + "<h1>");
-});
-
 app.get("/", (req, res) => {
-  res.send(
-    "<form action='/store-user' method='POST'> <label> Your Name </label><input type='text' name='username'><button>submit</button></form>"
-  );
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
 });
 
-app.post("/store-user", (req, res) => {
-  const username = req.body.username;
-
-  const filePath = path.join(__dirname, "data", "users.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const existingUsers = JSON.parse(fileData);
-
-  existingUsers.push(username);
-
-  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
-
-  res.send("<h1>Username stored!</h1>");
+app.get("/restaurants", (req, res) => {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
 });
 
-app.get("/show-users", (req, res) => {
-  const filePath = path.join(__dirname, "data", "users.json");
+app.get("/recommend", (req, res) => {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+});
 
-  const fileData = fs.readFileSync(filePath);
-  const existingUsers = JSON.parse(fileData);
+app.post("/recommend", (req, res) => {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const restaurantsData = JSON.parse(fs.readFileSync(filePath));
 
-  let responseData = "<ul>";
-  for (const user of existingUsers) {
-    responseData += "<li>" + user + "</li>";
-  }
-  responseData += "</ul>";
+  restaurantsData.push(restaurant);
+  fs.writeFileSync(filePath, JSON.stringify(restaurantsData));
 
-  res.send("<div>" + responseData + "</div>");
+  res.redirect("/confirm");
+});
+
+app.get("/about", (req, res) => {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/confirm", (req, res) => {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
 });
 
 app.listen(3000);
